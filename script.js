b77bcec77a190c53f521ff704a12d24625286c97
niconez <65620134+niconez@users.noreply.github.com>
2022-09-16 20:16:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//set active menu
+var header = document.getElementById("navLink");
+var links = header.getElementsByClassName("link");
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
